d495feec1bf5bf1e0877fa9b5226d7781b9609e8
Erick Cardenas <satanicodr@gmail.com>
2022-08-29 11:28:17 -0400

    Create Create_db.sh

---
+++ Create_db.sh
@@ -0,0 +1,15 @@
+# This is the main script that compiles the data from the M5P pipeline into a relational database
+
+# Process abundance table
+
+# Process annotation tables
+
+# Process assembly statistics
+
+# Process expression data
+
+# Query Pubmed for literature and upload to database
+
+# Query Patent database and upload to database
+
+# Query metabolism database
